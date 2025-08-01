56e2ead069925539f12fc45cfb6d5808fea81581
batosgi <novak34291@gmail.com>
2025-08-02 02:03:03 +0300

    bump version to 1.2.0 and implement absolute mouse movement functionality

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,8 +1,11 @@
 use std::{thread, time::Duration};
 
+use windows_sys::Win32::{Foundation::POINT, UI::WindowsAndMessaging::GetCursorPos};
+
 use crate::device::Device;
 
 #[repr(u8)]
+#[derive(Copy, Clone)]
 pub enum MouseButton {
     Left = 1,
     Right = 2,
@@ -28,16 +31,63 @@ impl<'a> Mouse<'a> {
     pub const fn new(device: &'a mut Device) -> Self {
         Self { device }
     }
-    
+
     pub fn click(&mut self, button: MouseButton, millis: u64) {
         self.device.send_mouse(button, 0, 0, 0);
         thread::sleep(Duration::from_millis(millis));
         self.device.send_mouse(MouseButton::Release, 0, 0, 0);
     }
 
-    pub fn move_absolute(&mut self, button: MouseButton, x: i8, y: i8) {
-        self.device.send_mouse(button, x, y, 0);
-        todo!("Absolute move is not yet implemented");
+    pub fn move_absolute(&mut self, button: MouseButton, x: u16, y: u16, millis: u64) {
+        // Get current mouse position
+        let mut current_point = POINT::default();
+        unsafe { GetCursorPos(&raw mut current_point) };
+
+        // Calculate deltas
+        let delta_x = i32::from(x) - current_point.x;
+        let delta_y = i32::from(y) - current_point.y;
+
+        // Calculate the number of steps and step sizes for both X and Y
+        let (count_x, mut x_step): (i32, i8) = if current_point.x > i32::from(x) {
+            (delta_x / -127, -127)
+        } else {
+            (delta_x / 127, 127)
+        };
+
+        let (count_y, mut y_step): (i32, i8) = if current_point.y > i32::from(y) {
+            (delta_y / -127, -127)
+        } else {
+            (delta_y / 127, 127)
+        };
+
+        let (final_x, final_y);
+        if count_x > 0 || count_y > 0 {
+            // Determine which axis takes more steps
+            let count;
+            if count_x > count_y {
+                count = count_x;
+                y_step = (delta_y / count) as i8;
+            } else if count_y > count_x {
+                count = count_y;
+                x_step = (delta_x / count) as i8;
+            } else {
+                count = count_x; // or count_y, they are equal
+            }
+
+            final_x = (delta_x - (x_step as i32 * count)) as i8;
+            final_y = (delta_y - (y_step as i32 * count)) as i8;
+            // Perform the movement in steps
+            for _ in 0..count {
+                self.move_relative(button, x_step, y_step);
+                thread::sleep(Duration::from_millis(millis));
+            }
+        } else {
+            final_x = (delta_x) as i8;
+            final_y = (delta_y) as i8;
+        }
+
+        // Ensure the final move reaches the target
+        self.move_relative(button, final_x, final_y);
     }
 
     pub fn move_relative(&mut self, button: MouseButton, x: i8, y: i8) {
--- tests/mouse.rs
+++ tests/mouse.rs
@@ -44,13 +44,44 @@ fn test_move_relative() {
     let mut device = Device::try_new().unwrap();
     let mut mouse = Mouse::new(&mut device);
 
-    let mut start_point = POINT { x: 0, y: 0 };
-    let mut end_point = POINT { x: 0, y: 0 };
+    let mut start_point = POINT::default();
+    let mut current_point = POINT::default();
     unsafe { GetCursorPos(&raw mut start_point) };
     mouse.move_relative(MouseButton::Release, 1, 1);
     thread::sleep(Duration::from_millis(100));
-    unsafe { GetCursorPos(&raw mut end_point) };
+    unsafe { GetCursorPos(&raw mut current_point) };
 
-    assert_eq!(start_point.x + 1, end_point.x);
-    assert_eq!(start_point.y + 1, end_point.y);
+    assert_eq!(start_point.x + 1, current_point.x);
+    assert_eq!(start_point.y + 1, current_point.y);
+}
+
+#[test]
+fn test_move_absolute() {
+    let mut device = Device::try_new().unwrap();
+    let mut mouse = Mouse::new(&mut device);
+    let mut current_point = POINT::default();
+
+    mouse.move_absolute(MouseButton::Release, 500, 500, 10);
+    thread::sleep(Duration::from_millis(100));
+    unsafe { GetCursorPos(&raw mut current_point) };
+    assert_eq!(current_point.x, 500);
+    assert_eq!(current_point.y, 500);
+
+    mouse.move_absolute(MouseButton::Release, 600, 600, 10);
+    thread::sleep(Duration::from_millis(100));
+    unsafe { GetCursorPos(&raw mut current_point) };
+    assert_eq!(current_point.x, 600);
+    assert_eq!(current_point.y, 600);
+
+    mouse.move_absolute(MouseButton::Release, 750, 750, 10);
+    thread::sleep(Duration::from_millis(100));
+    unsafe { GetCursorPos(&raw mut current_point) };
+    assert_eq!(current_point.x, 750);
+    assert_eq!(current_point.y, 750);
+
+    mouse.move_absolute(MouseButton::Release, 1919, 1, 10);
+    thread::sleep(Duration::from_millis(100));
+    unsafe { GetCursorPos(&raw mut current_point) };
+    assert_eq!(current_point.x, 1919);
+    assert_eq!(current_point.y, 1);
 }
